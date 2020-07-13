6c01667ec5842a0a259bbbb21390cff3454fd126
Gus <gus.fouad@gmail.com>
2020-07-13 17:35:24 -0400

    fixing- mragin, padding, opening links

--- client/src/components/PortHeader.js
+++ client/src/components/PortHeader.js
@@ -14,12 +14,26 @@ const PortHome = () => {
 					<div className="header">
 						<h1>Gus Fouad</h1>
 						<span className="sexy-line" />
+						<div className=" header-container">
+							<div className="header-img first-card">
+								<img src={fullstack} />
+								<p>Full-stack Developer</p>
+							</div>
+							<div className="header-img second-card">
+								<img src={film} />
+								<p>Filmmaker</p>
+							</div>
+							<div className="header-img third-card">
+								<img src={chess} />
+								<p>Chess Champion</p>
+							</div>
+						</div>
 					</div>
 
-					<a href="http://www.github.com/gusfouad">
+					<a target="_blank" href="http://www.github.com/gusfouad">
 						<i className="fab fa-github  fa-3x" />
 					</a>
-					<a href="https://www.linkedin.com/in/gusfouad/">
+					<a target="_blank" href="https://www.linkedin.com/in/gusfouad/">
 						<i className="fab fa-linkedin fa-3x" />
 					</a>
 				</div>
@@ -28,26 +42,6 @@ const PortHome = () => {
 						I am a full-stack developer building responsive and accessible web applications. As a filmmaker
 						and a chess champion, I have excellent experience in teamwork and problem solving.
 					</p>
-				</div>
-				<div className=" header-container">
-					<div className="header-img first-card">
-						<img src={fullstack} />
-						<p>Full-stack Developer</p>
-					</div>
-					<div className="header-img second-card" />
-					<div className="header-img third-card" />
-					<div className="header-img first-card" />
-					<div className="header-img second-card">
-						<img src={film} />
-						<p>Filmmaker</p>
-					</div>
-					<div className="header-img third-card" />
-					<div className="header-img first-card" />
-					<div className="header-img second-card" />
-					<div className="header-img third-card">
-						<img src={chess} />
-						<p>Chess Champion</p>
-					</div>
 				</div>
 			</div>
 		</div>
--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -12,75 +12,87 @@ const Portfolio = () => {
 			<span className="sexy-line" />
 			<div className="portfolio-container">
 				<div className="row portfolio-card ">
-					<div className="col m12">
-						<div className="card card-config">
-							<span className="card-title">CloudFlix</span>
-							<div className="card-image">
-								<img src={cfThumbnail} className="port-img" />
-							</div>
-							<p>Full Stack</p>
-							<div className="card-content">
-								<p>
-									A movie watchlist application. The user can create a profile, search the movie
-									database for movies, and add it to their watchlist
-								</p>
-								<p className="hidden-text">
-									monthly subscriptions,affiliate programs, authentication, authorization, and course
-									tracking managementt.adding a bit more we need to add more
-								</p>
-							</div>
-							<div className="card-action">
-								<a href="http://www.gusfouad.com/cloudflix/home">View Site</a>
+					<a className="port-click" target="_blank" href="http://www.gusfouad.com/cloudflix/home">
+						<div>
+							<div className="card card-config">
+								<span className="card-title">CloudFlix</span>
+								<div className="card-image">
+									<img src={cfThumbnail} className="port-img" />
+								</div>
+								<p>Full Stack</p>
+								<div className="card-content">
+									<p>
+										A movie watchlist application. The user can create a profile, search the movie
+										database for movies, and add it to their watchlist
+									</p>
+									<p className="hidden-text">
+										monthly subscriptions,affiliate programs, authentication, authorization, and
+										course tracking managementt.adding a bit more we need to add more
+									</p>
+								</div>
+								<div className="card-action">
+									<a target="_blank" href="http://www.gusfouad.com/cloudflix/home">
+										View Site
+									</a>
+								</div>
 							</div>
 						</div>
-					</div>
+					</a>
 				</div>
 				<div className="row portfolio-card col m12">
-					<div>
-						<div className="card card-config">
-							<span className="card-title">DumbMoney</span>
-							<div className="card-image">
-								<img src={dmThumbnail} className="port-img" />
-							</div>
-							<p>Word Press</p>
-							<div className="card-content">
-								<p>
-									Mitchell is a wealth wizard that offers online courses for wealth building and day
-									trading, books, and live streams. The site offers one time products for sale,
-									monthly subscriptions,affiliate programs, authentication, authorization, and course
-									tracking management.
-								</p>
-								<p className="hidden-text">line</p>
-							</div>
-							<div className="card-action">
-								<a href="http://dumbmoney.ca">View Site</a>
+					<a className="port-click" target="_blank" href="http://dumbmoney.ca">
+						<div>
+							<div className="card card-config">
+								<span className="card-title">DumbMoney</span>
+								<div className="card-image">
+									<img src={dmThumbnail} className="port-img" />
+								</div>
+								<p>Word Press</p>
+								<div className="card-content">
+									<p>
+										Mitchell is a wealth wizard that offers online courses for wealth building and
+										day trading, books, and live streams. The site offers one time products for
+										sale, monthly subscriptions,affiliate programs, authentication, authorization,
+										and course tracking management.
+									</p>
+									<p className="hidden-text">line</p>
+								</div>
+								<div className="card-action">
+									<a target="_blank" href="http://dumbmoney.ca">
+										View Site
+									</a>
+								</div>
 							</div>
 						</div>
-					</div>
+					</a>
 				</div>
 				<div className="row portfolio-card col m12">
-					<div>
-						<div className="card card-config">
-							<span className="card-title">Juno Challenge</span>
-							<div className="card-image">
-								<img src={jThumbnail} className="port-img" />
-							</div>
-							<p>HTML & CSS</p>
-							<div className="card-content">
-								<p>
-									Juno tech-challenge that requires a responsive, accessible, and pixel perfect
-									development from design
-								</p>
-								<p className="hidden-text">
-									monthly subscriptions,affiliate programs, authentication, authorization, and course
-									tracking management and then som is this enough.lets add onre more
-								</p>
-							</div>
-							<div className="card-action">
-								<a href="http://localhost:3000/juno.html">View Site</a>
+					<a className="port-click" target="_blank" href="http://localhost:3000/juno.html">
+						<div>
+							<div className="card card-config">
+								<span className="card-title">Juno Challenge</span>
+								<div className="card-image">
+									<img src={jThumbnail} className="port-img" />
+								</div>
+								<p>HTML & CSS</p>
+								<div className="card-content">
+									<p>
+										Juno tech-challenge that requires a responsive, accessible, and pixel perfect
+										development from design
+									</p>
+									<p className="hidden-text">
+										monthly subscriptions,affiliate programs, authentication, authorization, and
+										course tracking management and then som is this enough.lets add onre more
+									</p>
+								</div>
+								<div className="card-action">
+									<a target="_blank" href="http://localhost:3000/juno.html">
+										View Site
+									</a>
+								</div>
 							</div>
 						</div>
-					</div>
+					</a>
 				</div>
 			</div>
 		</div>
